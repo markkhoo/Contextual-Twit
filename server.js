3f1ce72ba2eb50c893cb5dae278be42bd407c41f
Manuel Villasenor <manuelvillasenor@2@gamil.com>
2021-05-28 14:33:43 -0700

    server mongoose

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
+const mongoose = require("mongoose");
+
 const app = express();
 
 // Define middleware here
@@ -10,6 +12,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactcms");
 
 // Define API routes here
 
