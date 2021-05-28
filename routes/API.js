2ec8f85c532e7022e5959a1817752163f8e5cafb
javimarashall <jamondragon@ucdavis.edu>
2021-05-28 11:58:34 -0700

    api route done

--- routes/API.js
+++ routes/API.js
@@ -1,1 +1,24 @@
-//
+const router = require("express").Router();
+const postController = require("../../controllers/postController");
+
+
+// Matches with "/api/books"
+router
+    .route("/")
+    .get(postController.findAll);
+
+// Matches with "/api/books"
+router
+    .route("/")
+    .post(postController.create);
+
+// Matches with "/api/books/:id"
+router
+    .route("/:id")
+    .delete(postController.remove);
+
+// router
+//     .route("/google/:book")
+//     .get(postsController.googleBooks)
+
+module.exports = router;
