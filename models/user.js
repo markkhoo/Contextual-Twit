2d6b3e522a26010ba39f9921f7df7888a905b4f1
Manuel Villasenor <manuelvillasenor@2@gamil.com>
2021-05-28 11:50:13 -0700

    models (still needs the relationships)

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String },
+    tag: { type: String },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
