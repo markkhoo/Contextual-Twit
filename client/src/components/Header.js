add85a3d270b967a893fc55d0067813f7997d659
javimarashall <jamondragon@ucdavis.edu>
2021-05-30 17:01:21 -0700

    header component done

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,1 +1,32 @@
-//
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+function Header(props) {
+    return (
+        <div>
+            <nav>
+                <div class="nav-wrapper">
+                    <a href="#!" class="brand-logo">Logo</a>
+                    <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                    <ul class="right hide-on-med-and-down">
+                        <li><a href="sass.html">Sass</a></li>
+                        <li><a href="badges.html">Components</a></li>
+                        <li><a href="collapsible.html">Javascript</a></li>
+                        <li><a href="mobile.html">Mobile</a></li>
+                    </ul>
+                </div>
+            </nav>
+
+            <ul class="sidenav" id="mobile-demo">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">Javascript</a></li>
+                <li><a href="mobile.html">Mobile</a></li>
+            </ul>
+
+        </div>
+    )
+};
+
+export default Header;
