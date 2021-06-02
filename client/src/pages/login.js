1340bc309417163f27545811dda21646d0cf0cba
Manuel Villasenor <manuelvillasenor@2@gamil.com>
2021-06-02 12:21:49 -0700

    fixes

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -2,10 +2,91 @@ import Header from "../components/Header";
 import React, { useState } from "react";
 import "./login.css";
 
+const axios = require('axios')
+
+
+
 function Login() {
-    //const [search, setSearch] = useState("");
+    //register user
+    const [account1, setAccount1] = useState("");
+    const [account2, setAccount2] = useState("");
+    const [account3, setAccount3] = useState("");
+
+function handleSetInput1(event){
+    setAccount1(event.target.value)
+    console.log(account1)
+
+}
+
+function handleSetInput2(event){
+    setAccount2(event.target.value)
+    console.log(account2)
 
 
+}
+function handleSetInput3(event){
+    setAccount3(event.target.value)
+    console.log(account3)
+
+}
+
+const handleRegisterSubmit = (event)=>{
+    event.preventDefault()
+    console.log("hello")
+    axios.post("/api/register",{
+        username: account1,
+        email: account2,
+        password: account3
+    })
+    .then(function (response){
+        console.log(response)
+    })
+
+}
+
+//login user
+const [login1, setlogin1] = useState("");
+const [login2, setlogin2] = useState("");
+const [login3, setlogin3] = useState("");
+
+
+function handleSetLoginInput1(event){
+    setlogin1(event.target.value)
+    console.log(setlogin1)
+
+}
+
+
+function handleSetLoginInput2(event){
+    setlogin2(event.target.value)
+    console.log(setlogin2)
+
+}
+function handleSetLoginInput3(event){
+    setlogin3(event.target.value)
+    console.log(setlogin2)
+
+}
+
+
+function handleLogin(event){
+
+    event.preventDefault()
+
+
+    axios.post("/api/login",{
+        username: login1,
+        email: login2,
+        password: login3
+    })
+    .then(function (response){
+        console.log(response)
+        if (response.ok) {
+            document.location.replace('/');
+          }
+    })
+    console.log(login1,login2,login3)
+}
 
     return (
 
@@ -20,22 +101,25 @@ function Login() {
                             <form className="col s12">
                                 <div className="row">
                                     <div className="input-field col s6">
-                                        <input placeholder="" id="first_name" type="text" className="validate" />
-                                        <label for="first_name">Username</label>
+                                        <input onChange={handleSetLoginInput1} placeholder="Placeholder" id="first_name2" type="text" className="validate" />
+                                        <label for="first_name">Username:</label>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="input-field col s12">
-                                        <input id="email" type="email" className="validate" />
+                                        <input onChange={handleSetLoginInput2} id="email2" type="email" className="validate" />
                                         <label for="email">Email</label>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="input-field col s12">
-                                        <input id="password" type="password" className="validate" />
+                                        <input onChange={handleSetLoginInput3} id="password2" type="password" className="validate" />
                                         <label for="password">Password</label>
                                     </div>
                                 </div>
+                                <button onClick={handleLogin} className="btn waves-effect waves-light" type="submit" name="action">Submit
+                                            <i className="material-icons right">send</i>
+                                        </button>
                             </form>
                         </div>
                     </div>
@@ -47,22 +131,25 @@ function Login() {
                             <form className="col s12">
                                 <div className="row">
                                     <div className="input-field col s6">
-                                        <input placeholder="" id="first_name" type="text" className="validate" />
-                                        <label for="first_name">Username</label>
+                                        <input onChange={handleSetInput1} placeholder="Placeholder"  id="first_name" type="text" className="validate" />
+                                        <label for="first_name">Username:</label>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="input-field col s12">
-                                        <input id="email" type="email" className="validate" />
+                                        <input onChange={handleSetInput2} id="email" type="email" className="validate" />
                                         <label for="email">Email</label>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="input-field col s12">
-                                        <input id="password" type="password" className="validate" />
+                                        <input onChange={handleSetInput3} id="password" type="password" className="validate" />
                                         <label for="password">Password</label>
                                     </div>
                                 </div>
+                                <button onClick={handleRegisterSubmit} className="btn waves-effect waves-light" type="submit" name="action">Submit
+                                            <i className="material-icons right">send</i>
+                                        </button>
                             </form>
                         </div>
                     </div>
