de0941754eab316a1eae9287d383a03ee2832da5
Manuel Villasenor <manuelvillasenor@2@gamil.com>
2021-06-01 17:21:34 -0700

    login only needs redirect

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -5,7 +5,7 @@ const axios = require('axios')
 
 
 function Login() {
-    
+    //register user
     const [account1, setAccount1] = useState("");
     const [account2, setAccount2] = useState("");
     const [account3, setAccount3] = useState("");
@@ -28,8 +28,7 @@ function handleSetInput3(event){
 
 }
 
-
-const handleRegisterSubmit =(event)=>{
+const handleRegisterSubmit = (event)=>{
     event.preventDefault()
     console.log("hello")
     axios.post("/api/register",{
@@ -43,9 +42,48 @@ const handleRegisterSubmit =(event)=>{
 
 }
 
+//login user
+const [login1, setlogin1] = useState("");
+const [login2, setlogin2] = useState("");
+const [login3, setlogin3] = useState("");
 
-function handleLogin(){
 
+function handleSetLoginInput1(event){
+    setlogin1(event.target.value)
+    console.log(setlogin1)
+
+}
+
+
+function handleSetLoginInput2(event){
+    setlogin2(event.target.value)
+    console.log(setlogin2)
+
+}
+function handleSetLoginInput3(event){
+    setlogin3(event.target.value)
+    console.log(setlogin2)
+
+}
+
+
+function handleLogin(event){
+
+    event.preventDefault()
+
+
+    axios.post("/api/login",{
+        username: login1,
+        email: login2,
+        password: login3
+    })
+    .then(function (response){
+        console.log(response)
+        if (response.ok) {
+            document.location.replace('/');
+          }
+    })
+    console.log(login1,login2,login3)
 }
 
     return (
@@ -61,22 +99,25 @@ function handleLogin(){
                             <form className="col s12">
                                 <div className="row">
                                     <div className="input-field col s6">
-                                        <input placeholder="Placeholder" id="first_name" type="text" className="validate" />
+                                        <input onChange={handleSetLoginInput1} placeholder="Placeholder" id="first_name2" type="text" className="validate" />
                                         <label for="first_name">Username:</label>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="input-field col s12">
-                                        <input id="email" type="email" className="validate" />
+                                        <input onChange={handleSetLoginInput2} id="email2" type="email" className="validate" />
                                         <label for="email">Email</label>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="input-field col s12">
-                                        <input id="password" type="password" className="validate" />
+                                        <input onChange={handleSetLoginInput3} id="password2" type="password" className="validate" />
                                         <label for="password">Password</label>
                                     </div>
                                 </div>
+                                <button onClick={handleLogin} className="btn waves-effect waves-light" type="submit" name="action">Submit
+                                            <i className="material-icons right">send</i>
+                                        </button>
                             </form>
                         </div>
                     </div>
