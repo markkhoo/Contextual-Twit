7035e8164f56e83b2ecc07dcc4adb1d4d285a551
javimarashall <jamondragon@ucdavis.edu>
2021-06-06 16:46:39 -0700

    pushing to pull Rosario's edits

--- server.js
+++ server.js
@@ -30,14 +30,6 @@ app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactcms");
 
-// Define API routes here
-
-// Send every other request to the React app
-// Define any API routes before this runs
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
-
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
